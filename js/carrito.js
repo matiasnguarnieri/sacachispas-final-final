c4b2f3a1b73cb1a89fcbaac6bc68907e49b455c5
Ailu Vilches <ailenpereiravilches@gmail.com>
2022-11-13 18:46:29 -0300

    calculartotal

--- js/carrito.js
+++ js/carrito.js
@@ -3,26 +3,28 @@ let carritoLleno = JSON.parse(localStorage.getItem("carrito"));
 
 
     carritoLleno.forEach((item)=>{
+        let total =document.getElementById("total")
         let tabla = document.getElementById("tablaCarrito");
-        tabla.innerHTML=`
-        <tr>
+        let fila = document.createElement("tr");
+        fila.innerHTML=`
+        
         <td>${item.producto}</td>
         <td>${item.talle}</td>
         <td>${item.cantidad}</td>
         <td>$${item.precio}</td>
         <td>$${item.cantidad*item.precio}</td>
         <td><i class="fa-solid fa-trash tachito"></i></td>
-        </tr>
+        
         `;
+
+        tabla.insertBefore(fila, total);
 
         console.log(tabla);
         console.log(carritoLleno);
+        calcularTotal();
     });
 
-    carritoLleno.forEach((item2)=>{
-        let contenedor = document.querySelector("#contenedor_tituloCarrito");
-        contenedor.innerHTML=`
-        <p>${item2.producto}</p>
-        `;
-    }
-    );
+ function calcularTotal(){
+    let totalPrecio =document.getElementById("precioTotal")
+    totalPrecio.innerText=
+ }
