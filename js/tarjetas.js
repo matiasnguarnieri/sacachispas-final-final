0ec02915473d228c7b51667b9bc68f4df580cb12
Mati <matiasnguarnieri@hotmail.com>
2022-11-15 01:51:24 -0300

    prueba tarjetas

---
+++ js/tarjetas.js
@@ -0,0 +1,24 @@
+// const nroTarjeta = document.getElementById("numero-tarjeta");
+// const mensajeError = document.getElementById("unDiv");
+
+let totalNueveDigitos = 0;
+let valorNro = "2222222228";
+let valorTxt = valorNro.toString();
+let ultNro = valorTxt.charAt(valorTxt.length - 1);
+
+validacionNros();
+
+function sumaNueveRestantes(){
+    for (let i = 0; i < valorTxt.length-1; i++) {
+        totalNueveDigitos += parseInt(valorTxt[i], 10);
+    }
+}
+
+function validacionNros(){
+    sumaNueveRestantes();
+    if(ultNro%2 == 0 && totalNueveDigitos%2 == 0 || ultNro%2 !== 0 && totalNueveDigitos%2 !== 0 ){
+        alert("NroTarjeta invalido");
+    }else{
+        alert("NroTarjeta valido");
+    }
+}
