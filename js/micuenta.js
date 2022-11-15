8331456dfc614a69885dcf56eab1a08f302b67a9
Ailu Vilches <ailenpereiravilches@gmail.com>
2022-11-15 00:23:17 -0300

    agregué tarjetas

--- js/micuenta.js
+++ js/micuenta.js
@@ -10,6 +10,8 @@ botonCerrarPopUpDireccion.addEventListener("click", ()=>{
     popUpDirecciones.classList.add("noShow");
 });
 
+let direcciones = [];
+
 const formDirec = document.getElementById("formDirecciones");
 const tablaDirecciones = document.getElementById("tablaMisDirecciones");
 const alias = document.getElementById("alias");
@@ -26,9 +28,83 @@ formDirec.addEventListener("submit", function(event){
     nuevaLinea.innerHTML = `<i class="fa-solid fa-trash tachito" id="tachito" onclick="eliminarFila(${numerito})"></i>`;
     popUpDirecciones.classList.add("noShow");
     numerito++;
+    direcciones.push({
+        alias : alias.value,
+        direccion : direc.value
+    }
+    );
+    console.log(direcciones);
+    guardarDirecionesEnLocalStorage();
 });
+
+const guardarDirecionesEnLocalStorage = ()=>{
+    localStorage.setItem("direcciones", JSON.stringify(direcciones));
+};
 
 function eliminarFila(a){
     tablita.deleteRow(a);
     numerito--;
 }
+
+//Tarjetas
+const botonAgregarTarjeta = document.getElementById("botonAgregarTarjeta");
+const popUpTarjetas = document.getElementById("pagar-otra-tarjeta nueva-tarjeta");
+const botonCerrarPopUpTarjeta = document.getElementById("botonCerrarTarjeta");
+botonAgregarTarjeta.addEventListener("click", ()=>{
+    popUpTarjetas.classList.remove("noShow");
+});
+botonCerrarPopUpTarjeta.addEventListener("click", ()=>{
+    popUpTarjetas.classList.add("noShow");
+});
+
+let tarjetas = [];
+if(localStorage.getItem("tarjetas")!==null){
+    tarjetas=JSON.parse(localStorage.getItem("tarjetas"));;
+};
+
+function guardarTarjetas(){
+    const aliasTarjeta = document.getElementById("aliasTarjeta");
+    const titular = document.getElementById("titular-nombre");
+    tarjetas.push({
+        alias : aliasTarjeta.value,
+        titular : titular.value
+    }
+    );
+    guardarTarjetasEnLocalStorage();
+    popUpTarjetas.classList.add("noShow");
+};
+
+const guardarTarjetasEnLocalStorage = ()=>{
+    localStorage.setItem("tarjetas", JSON.stringify(tarjetas));
+};
+
+    let tarjetasGuardadas = JSON.parse(localStorage.getItem("tarjetas"));
+    tarjetasGuardadas.forEach((item)=>{
+    let tabla = document.getElementById("tablaTarjetas");
+    let fila = document.createElement("tr");
+    fila.innerHTML=`
+    <td>${item.alias}</td>
+    <td>${item.titular}</td>
+    <td><i class="fa-solid fa-trash tachito" id="tachito"></i></td> 
+    `;
+    
+    tabla.append(fila);
+    let eliminar = fila.querySelector(".fa-solid.fa-trash.tachito");
+    eliminar.addEventListener("click", ()=>{
+        eliminarProducto(item.alias);
+        navigation.reload();
+    });
+    }
+    );
+
+    const eliminarProducto = (alias) => {
+        const foundAlias = tarjetasGuardadas.find((element) => element.alias === alias);
+         
+        tarjetasGuardadas = tarjetasGuardadas.filter((tarjetaAlias) => {
+            return tarjetaAlias !== foundAlias;
+        });
+        console.log(tarjetasGuardadas);
+        localStorage.setItem("tarjetas", JSON.stringify(tarjetasGuardadas));
+    };
+
+
